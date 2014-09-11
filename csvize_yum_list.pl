d757352741048fe88467b50e9a7d8b21536f75e3
dpecka <dpecka@opensuse.org>
2014-09-11 16:53:09 +0200

    commit-1

---
+++ csvize_yum_list.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+#
+# usage: yum -q list --showduplicates | /path/to/this/script
+#
+
+use strict;
+use warnings;
+
+chomp(my @__input = <>);
+my @_postprocessed_input;
+
+my $counter = "-1";
+while(@__input) {
+        $counter++;
+        my $item = shift(@__input);
+        if($item =~ /^\s+/) {
+                $_postprocessed_input[-1] .= "\t$item";
+                next;
+
+        };
+        push(@_postprocessed_input, $item);
+};
+
+undef(@__input);
+
+my $_pkg_status = "?";
+
+foreach(@_postprocessed_input) {
+        if(/^Installed/) { $_pkg_status = "i"; next; };
+        if(/^Available/) { $_pkg_status = "a"; next; };
+        print join(";", split(/\s+/), "$_pkg_status"), "\n";
+};
+
+exit 0;
