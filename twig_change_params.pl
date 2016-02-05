6c1a5407301df151ba482b4a1d482052d9ab30ce
dpecka <dpecka@opensuse.org>
2016-02-05 15:11:32 +0100

    some example xml processing with perl XML::Twig

---
+++ twig_change_params.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+#
+# use as:
+# ./twig_change_params.pl '/Property/Property[@id="fuj"]' muehehe haha
+#
+
+use strict;
+use warnings;
+use XML::Twig;
+
+my $twig = XML::Twig->new(
+	pretty_print => 'indented',
+);
+
+
+$twig->parsefile($ARGV[0]) || die "can't open file: $ARGV[0]\n";
+
+my ($_item_to_change) = $twig->findnodes($ARGV[1]);
+
+$_item_to_change->set_att(
+	$ARGV[2] => $ARGV[3],
+
+);
+
+$twig->flush;
+
+
+exit 0;
+
---
+++ twig_change_values.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+#
+# use as:
+# ./twig_change_values.pl '/Property/Property[@id="1"]/Property' ueee
+#
+
+use strict;
+use warnings;
+use XML::Twig;
+
+my $twig = XML::Twig->new(
+	pretty_print => 'indented',
+);
+
+
+$twig->parsefile($ARGV[0]) || die "can't open file: $ARGV[0]\n";
+
+my ($_item_to_change) = $twig->findnodes($ARGV[1]);
+
+$_item_to_change->set_text(
+	$ARGV[2],
+
+);
+
+$twig->flush;
+
+
+exit 0;
+
